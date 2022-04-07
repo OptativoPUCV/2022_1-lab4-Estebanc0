d6fb4212f94f614aefe878fb040b9c07f2f99c10
estebanc4rrasco <>
2022-04-07 15:37:54 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -56,7 +56,7 @@ HashMap * createMap(long capacity) {
   new=(HashMap*)malloc(capacity*sizeof(HashMap));
   new->current=-1;
   new->capacity=capacity;
-  new->buckets[NULL];
+  new->buckets[10];
   return new;
 }
 
