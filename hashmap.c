95ba08ea536b1abe0dbe570f32c272af625b19f4
Your Name <you@example.com>
2022-04-10 12:05:05 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -99,7 +99,7 @@ Pair * searchMap(HashMap * map,  char * key) {
 
 Pair * firstMap(HashMap * map) {
     for (long idx=0; idx<map->capacity;idx++){
-        if(map->buckets[idx]->key!=NULL && map->buckets[idx]!=NULL){
+        if(map->buckets[idx]!=NULL && map->buckets[idx]->key!=NULL ){
             map->current=idx;
             return map->buckets[idx];
         }
